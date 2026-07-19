68d139dbafcd4b9bc11756b18cc91672029ce01c
Priyanshi Solanki <priyanshi.solanki12006@gmail.com>
2026-07-19 23:06:01 +0530

    Day 06: Solved 546A - Soldier and Bananas

---
+++ Day06/546A_Soldier_and_Bananas/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+        int total = 0;
+        for (int i = 1; i <= w; i++) {
+            total += i * k;
+        }
+        if (total > n)
+            System.out.println(total - n);
+        else
+            System.out.println(0);
+    }
+}
