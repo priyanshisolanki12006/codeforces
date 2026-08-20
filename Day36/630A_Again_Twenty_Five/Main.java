9f3ca95ed16571afd80310e5b0e97d4dffe4fbf0
Priyanshi Solanki <priyanshi.solanki12006@gmail.com>
2026-08-20 22:54:08 +0530

    Day 36: Solved 630A — Again Twenty Five

---
+++ Day36/630A_Again_Twenty_Five/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        long n = read.nextLong();
+        System.out.println(25);
+    }
+}
