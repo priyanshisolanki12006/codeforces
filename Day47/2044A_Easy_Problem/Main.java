ce74cfdcbcd35ff93821d065fd849faf373ba95a
Priyanshi Solanki <priyanshi.solanki12006@gmail.com>
2026-08-31 20:46:08 +0530

    Day 47: 2044A - Easy Problem

---
+++ Day47/2044A_Easy_Problem/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        int t = read.nextInt();
+        while(t-->0){
+            int n = read.nextInt();
+            System.out.println(n-1);
+        }
+    }
+}
