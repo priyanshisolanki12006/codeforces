61db0d5ae34ef1b02e8af7cc886354a3ec6c785a
Priyanshi Solanki <priyanshi.solanki12006@gmail.com>
2026-09-07 21:25:56 +0530

    Day 56: 50A - Domino piling

---
+++ Day56/50A_Domino_piling/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        int m = read.nextInt();
+        int n = read.nextInt();
+        System.out.println((m*n)/2);
+    }
+}
